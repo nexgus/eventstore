1a81ce0e51d52e91039c64528e561fda6fbbde9d
Augustus Chen <augustus.sanchung@gmail.com>
2021-05-24 13:11:18 +0800

    Ensure Get() is in sequence

--- eventstore.go
+++ eventstore.go
@@ -3,6 +3,7 @@ package eventstore
 import (
 	"encoding/binary"
 	"fmt"
+	"sort"
 	"time"
 
 	badger "github.com/dgraph-io/badger/v3"
@@ -53,20 +54,40 @@ func (es *EventStore) get(
 	txn *badger.Txn,
 	fn func(val []byte) error,
 ) ([]byte, error) {
-	opts := badger.DefaultIteratorOptions
-	opts.PrefetchValues = false
-	iter := txn.NewIterator(opts)
-	defer iter.Close()
 
-	var key []byte
-	for iter.Rewind(); iter.Valid(); iter.Next() {
-		item := iter.Item()
+	var keys []int64
+	es.db.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchSize = 10
+		iter := txn.NewIterator(opts)
+		defer iter.Close()
 
+		for iter.Rewind(); iter.Valid(); iter.Next() {
+			item := iter.Item()
+			key := int64(binary.LittleEndian.Uint64(item.Key()))
+			keys = append(keys, key)
+		}
+
+		return nil
+	})
+
+	if len(keys) == 0 {
+		return nil, nil
+	}
+
+	if len(keys) > 2 {
+		sort.Slice(keys, func(i, j int) bool {
+			return keys[i] < keys[j]
+		})
+	}
+
+	key := make([]byte, 8)
+	binary.LittleEndian.PutUint64(key, uint64(keys[0]))
+	if item, err := txn.Get(key); err != nil {
+		return key, fmt.Errorf("get: %v", err)
+	} else {
 		if err := item.Value(fn); err != nil {
-			return nil, err
-		} else {
-			key = item.Key()
-			break
+			return key, fmt.Errorf("value: %v", err)
 		}
 	}
 
